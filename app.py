3fd8eacc2473fa646bff32af040fb54c138c12bc
lsampaio13 <sampaio.luisa13@gmail.com>
2026-09-03 23:00:50 -0300

    tabela de pedido no model.py, mudei a tabela de cliente (id -> id_cliente)

--- app.py
+++ app.py
@@ -48,6 +48,34 @@ def pedido():
                                mensagem=f"Pedido cadastrado com sucesso!")
     return render_template('cadastro_restaurante.html', mensagem=None)
 
+@app.route('/pedido', methods=['GET', 'POST'])
+def pedido():
+    if request.method == 'GET':
+
+        query = '''
+                SELECT co.id_contato, co.nome, co.email, ca.data_cadastro,
+                       t.telefone_res, t.telefone_co, t.telefone_cel,
+                       GROUP_CONCAT(cat.tipo_cadastro, ', ') as tipos
+                FROM contato co
+                LEFT JOIN telefones t ON co.id_telefone = t.id_telefone
+                LEFT JOIN cadastro ca ON co.id_contato = ca.id_contato
+                LEFT JOIN categoria cat ON co.id_contato = cat.id_contato
+                GROUP BY co.id_contato
+                ORDER BY co.nome
+            '''
+
+        conn = sqlite3.connect('restaurante.db')
+        cursor = conn.cursor()
+            
+            
+            cursor.execute(query)
+            pedidos = cursor.fetchall()
+            conn.close()
+    
+    return render_template('listar.html', pedidos=pedidos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
--- model.py
+++ model.py
@@ -5,9 +5,21 @@ cursor = conn.cursor()
 
 cliente = """ 
     CREATE TABLE IF NOT EXISTS cliente (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         telefone TEXT NOT NULL
     );
 """
+
+pedidos = '''
+    CREATE TABLE IF NOT EXISTS pedidos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_cliente INTEGER,
+    nome_prato TEXT NOT NULL,
+    valor_unitario DOUBLE NOT NULL,
+    qtd_pratos INTEGER NOT NULL,
+    valor_total DOUBLE NOT NULL
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) ON DELETE CASCADE
+    );
+    '''
