99e87c3ee61e192153bdd50bca8563a70fe80c45
lsampaio13 <sampaio.luisa13@gmail.com>
2026-09-04 19:49:58 -0300

    CRUD completo do cliente

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ def pedido():
                                mensagem=f"Pedido cadastrado com sucesso!")
     return render_template('cadastro_pedido.html', mensagem=None)
 
+#------------------------pedido------------------------------
+
 @app.route('/listar-pedidos')
 def listar_pedidos():
     conn = sqlite3.connect('restaurante.db')
@@ -122,6 +124,76 @@ def excluir_pedido(id_pedido):
     conn.close()
 
     return redirect(url_for('listar_pedidos'))
+
+
+#-------------------------cliente------------------------------
+
+@app.route('/listar-clientes')
+def listar_clientes():
+    conn = sqlite3.connect('restaurante.db')
+    cursor = conn.cursor()
+
+    query = '''
+        SELECT c.id_cliente, c.nome, c.email, c.telefone
+        FROM clientes c
+        ORDER BY c.nome
+    '''
+
+    cursor.execute(query)
+    clientes = cursor.fetchall()
+    conn.close()
+
+    return render_template('listar_clientes.html', clientes = clientes)
+
+@app.route('/editar-cliente/<int:id_cliente>', methods=['GET', 'POST'])
+def editar_cliente(id_cliente):
+    conn = sqlite3.connect('restaurante.db')
+    cursor = conn.cursor()
+
+    query = '''
+        SELECT c.id_cliente, c.nome, c.email, c.telefone
+        FROM clientes c
+        WHERE c.id_cliente = ?
+    '''
+    cursor.execute(query, (id_cliente,))
+    cliente = cursor.fetchone()
+    conn.commit()
+    conn.close()
+    
+    return render_template('cadastro_cliente.html', modo='editar', cliente=cliente)
+
+    if request.method == 'POST':
+
+        nome = request.form['nome']
+        email = request.form['email']
+        telefone = request.form['telefone']
+
+        conn = sqlite3.connect('restaurante.db')
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            UPDATE clientes
+            SET id_cliente = ?, nome  = ?, email = ?, telefone = ?
+            WHERE id_cliente = ?
+        ''', (id_cliente, nome, email, telefone))
+
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('listar_clientes'))
+
+@app.route('/excluir-cliente/<int:id_cliente>', methods=['POST'])
+def excluir_cliente(id_cliente):
+    conn = sqlite3.connect('restaurante.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('DELETE FROM clientes WHERE id_cliente = ?', (id_cliente,))
+    
+    conn.commit()
+    conn.close()
+    
+    return redirect(url_for('listar_clientes'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
